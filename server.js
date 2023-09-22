5e124c4d112bf57eedecfcb51ae4af2beed38205
mortennordseth <morten@nordseth.net>
2023-09-22 07:56:05 +0200

    build: dockerfile and docker build push deploy workflow

---
+++ server.js
@@ -0,0 +1,15 @@
+const orgId = process.env.NEXT_PUBLIC_PLANNER_ORG_ID || 'atb';
+
+switch (orgId) {
+  case 'atb':
+    require('./dist/atb/standalone/server.js');
+    break;
+  case 'nfk':
+    require('./dist/nfk/standalone/server.js');
+    break;
+  case 'fram':
+    require('./dist/fram/standalone/server.js');
+    break;
+  default:
+    throw new Error('Invalid org ID provided.');
+}
