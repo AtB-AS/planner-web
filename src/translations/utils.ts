c2eeeb28b98daa9a833019f51e4e5124f233e426
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-09-22 22:23:53 +0200

    feat: add initial version of search bar

--- src/translations/utils.ts
+++ src/translations/utils.ts
@@ -1,4 +1,4 @@
-import { WEBSHOP_ORGS } from '@atb/modules/org-data';
+import { currentOrg, type WEBSHOP_ORGS } from '@atb/modules/org-data';
 import merge from 'lodash/merge';
 
 export type LanguageOverrides<T> = {
@@ -7,7 +7,7 @@ export type LanguageOverrides<T> = {
 export function orgSpecificTranslations<T>(
   translationTexts: T,
   overrides: Partial<{ [org in WEBSHOP_ORGS]: LanguageOverrides<T> }>,
-  orgId: WEBSHOP_ORGS = 'atb',
+  orgId: WEBSHOP_ORGS = currentOrg,
 ) {
   return merge({}, translationTexts, overrides[orgId]);
 }
