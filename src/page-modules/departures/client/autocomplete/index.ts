076c9351164b63dacf2e5d7ff524b965d3dd6024
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-09-26 14:53:06 +0200

    refactor: add hook for debouncing

--- src/page-modules/departures/client/autocomplete/index.ts
+++ src/page-modules/departures/client/autocomplete/index.ts
@@ -1,12 +1,15 @@
 import { swrFetcher } from '@atb/modules/api-browser';
 import { AutocompleteFeature } from '../../types';
 import useSWR from 'swr';
+import useDebounce from '@atb/utils/use-debounce';
 
 export type AutocompleteApiReturnType = AutocompleteFeature[];
 
 export function useAutocomplete(q: string) {
+  const debouncedQuery = useDebounce(q, 500);
+
   return useSWR<AutocompleteApiReturnType>(
-    `/api/departures/autocomplete?q=${q}`,
+    `/api/departures/autocomplete?q=${debouncedQuery}`,
     swrFetcher,
   );
 }
