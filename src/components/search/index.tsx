8f60307124b0cbf3572665c71750ca78761bc340
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-09-26 16:26:31 +0200

    refactor: refactor search highlighting

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -2,7 +2,6 @@ import Downshift from 'downshift';
 import { useState } from 'react';
 import { useAutocomplete } from '@atb/page-modules/departures/client';
 import style from './search.module.css';
-import { debounce, split } from 'lodash';
 import { MonoIcon } from '@atb/assets/mono-icon';
 import VenueIcon from '@atb/components/venue-icon';
 import { andIf } from '@atb/utils/css';
@@ -16,16 +15,10 @@ type SearchProps = {
 export default function Search({
   label,
   onChange,
-  initialQuery = '',
+  initialQuery = 'No',
 }: SearchProps) {
   const [query, setQuery] = useState(initialQuery);
   const { data } = useAutocomplete(query);
-
-  const highlight = (name: string, inputValue: string | null) => {
-    if (!inputValue) return [name];
-
-    return split(name, new RegExp(`(${inputValue})`, 'gi'));
-  };
 
   return (
     <Downshift
@@ -66,40 +59,63 @@ export default function Search({
           </button>
 
           <ul className={style.menu} {...getMenuProps()}>
-            {isOpen && data
-              ? data.map((item, index) => (
-                  <li
-                    className={andIf({
-                      [style.item]: true,
-                      [style.itemHighlighted]: highlightedIndex === index,
-                    })}
-                    {...getItemProps({
-                      index,
-                      item,
-                    })}
-                    key={item.id}
-                  >
-                    <div className={style.itemIcon} aria-hidden>
-                      <VenueIcon category={item.category} />
-                    </div>
-                    <span className={style.itemName}>
-                      {highlight(item.name, inputValue).map((part, index) => {
-                        if (!part.length || !inputValue) return;
-                        return part.toLowerCase() ===
-                          inputValue.toLowerCase() ? (
-                          <strong key={index}>{part}</strong>
-                        ) : (
-                          <span key={index}>{part}</span>
-                        );
-                      })}
-                    </span>
-                    <span className={style.itemLocality}>{item.locality}</span>
-                  </li>
-                ))
-              : null}
+            {isOpen &&
+              data?.map((item, index) => (
+                <li
+                  className={andIf({
+                    [style.item]: true,
+                    [style.itemHighlighted]: highlightedIndex === index,
+                  })}
+                  key={item.id}
+                  {...getItemProps({
+                    index,
+                    item,
+                  })}
+                >
+                  <div className={style.itemIcon} aria-hidden>
+                    <VenueIcon category={item.category} />
+                  </div>
+                  <span className={style.itemName}>
+                    {highlightSearchText(inputValue, item.name).map(
+                      ({ part, highlight }) => {
+                        if (highlight)
+                          return <strong key={item.id + part}>{part}</strong>;
+                        else return <span key={item.id + part}>{part}</span>;
+                      },
+                    )}
+                  </span>
+                  <span className={style.itemLocality}>{item.locality}</span>
+                </li>
+              ))}
           </ul>
         </div>
       )}
     </Downshift>
   );
+}
+
+function highlightSearchText(input: string | null, name: string) {
+  if (!input) return [{ part: name, highlight: false }];
+
+  let startIndex = name.toLowerCase().indexOf(input.toLowerCase());
+
+  if (startIndex !== -1) {
+    let endIndex = startIndex + input.length;
+    return [
+      {
+        part: name.substring(0, startIndex),
+        highlight: false,
+      },
+      {
+        part: name.substring(startIndex, endIndex),
+        highlight: true,
+      },
+      {
+        part: name.substring(endIndex),
+        highlight: false,
+      },
+    ];
+  } else {
+    return [{ part: name, highlight: false }];
+  }
 }
