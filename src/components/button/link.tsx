87cc8e25025e5293bc1ca1b4ea40a4ddc8766297
mortennordseth <morten@nordseth.net>
2023-09-29 17:29:38 +0200

    feat: button components

---
+++ src/components/button/link.tsx
@@ -0,0 +1,75 @@
+import Link from 'next/link';
+import React from 'react';
+import {ButtonBase, ButtonBaseProps, getBaseButtonClassName} from './utils';
+
+import {UrlObject} from 'url';
+
+export type ButtonLinkProps = {
+  /**
+   * Link or UrlObject passed to next/link
+   */
+  href: string | UrlObject;
+  /**
+   * Optional additional onClick if you want to
+   * enrich click actions with JavaScript function
+   */
+  onClick?: JSX.IntrinsicElements['a']['onClick'];
+  /**
+   * Specify properties to underlying a tag
+   */
+  aProps?: JSX.IntrinsicElements['a'];
+  /**
+   * If we should do shallow routing or not.
+   * Shallow routing will not trigger server side props
+   * @default false
+   */
+  shallow?: boolean;
+} & ButtonBaseProps;
+
+export function ButtonLink({
+  href,
+  onClick,
+  testID,
+  aProps = {},
+  shallow = false,
+  ...props
+}: ButtonLinkProps) {
+  const className = getBaseButtonClassName(props);
+  const extraProps =
+    props.state == 'active'
+      ? {
+          'aria-current': true,
+        }
+      : {};
+
+  if (props.disabled || props.state == 'loading') {
+    return (
+      <a
+        className={className}
+        role="link"
+        aria-disabled
+        data-testid={testID}
+        {...extraProps}
+        {...aProps}
+      >
+        <ButtonBase {...props} />
+      </a>
+    );
+  }
+
+  return (
+    <Link href={href} shallow={shallow} legacyBehavior>
+      <a
+        className={className}
+        onClick={onClick}
+        {...extraProps}
+        {...aProps}
+        data-testid={testID}
+      >
+        <ButtonBase {...props} />
+      </a>
+    </Link>
+  );
+}
+
+export default ButtonLink;
