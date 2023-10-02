aeee33bd8e1bc18843dc788d6bd0b30b86e62420
Mikael Brevik <mikaelbre@gmail.com>
2023-10-02 15:42:25 +0200

    chore: change from ts to yaml for graphql

--- graphql.config.ts
+++
@@ -1,9 +0,0 @@
-import type { IGraphQLConfig } from 'graphql-config';
-
-const config: IGraphQLConfig = {
-  schema: 'https://api.entur.io/journey-planner/v3/graphql',
-  documents: 'src/**/*.gql',
-  exclude: ['src/**/*.generated.ts'],
-};
-
-export default config;
