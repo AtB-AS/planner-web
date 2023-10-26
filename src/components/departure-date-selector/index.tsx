787dbfa8d4cc89dffe24886a1e1d709cbe6eb8ab
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-10-26 11:22:44 +0200

    style: add animation to departure date selector (#40)

--- src/components/departure-date-selector/index.tsx
+++ src/components/departure-date-selector/index.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useState } from 'react';
 import style from './departure-date-selector.module.css';
 import { ComponentText, Language, useTranslation } from '@atb/translations';
 import { TFunc } from '@leile/lobo-t';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export enum DepartureDateState {
   Now = 'now',
@@ -89,49 +90,73 @@ export default function DepartureDateSelector({
     <div className={style.departureDateSelector}>
       <div className={style.options}>
         {Object.values(DepartureDateState).map((state) => (
-          <div key={state} className={style.option}>
-            <label>
-              <input
-                type="radio"
-                name="departureDateSelector"
-                value={state}
-                checked={selectedOption.type === state}
-                onChange={() => internalOnStateChange(state)}
-                aria-label={stateToLabel(state, t)}
-              />
-              <span aria-hidden>{stateToLabel(state, t)}</span>
-            </label>
-          </div>
+          <label key={state} className={style.option}>
+            <input
+              type="radio"
+              name="departureDateSelector"
+              value={state}
+              checked={selectedOption.type === state}
+              onChange={() => internalOnStateChange(state)}
+              aria-label={stateToLabel(state, t)}
+              className={style.option__input}
+            />
+
+            <span aria-hidden className={style.option__label}>
+              {selectedOption.type === state && (
+                <motion.span
+                  layoutId="departureDateSelector"
+                  className={style.option__selected}
+                />
+              )}
+              <span className={style.option__text}>
+                {stateToLabel(state, t)}
+              </span>
+            </span>
+          </label>
         ))}
       </div>
 
-      {selectedOption.type !== DepartureDateState.Now && (
-        <div className={style.dateAndTimeSelectors}>
-          <div className={style.dateSelector}>
-            <label htmlFor="departureDateSelector">
-              {t(ComponentText.DepartureDateSelector.date)}
-            </label>
-            <input
-              type="date"
-              id="departureDateSelector"
-              value={selectedDate.toISOString().slice(0, 10)}
-              onChange={internalOnDateChange}
-            />
-          </div>
-          <div className={style.timeSelector}>
-            <label htmlFor="departureTimeSelector">
-              {t(ComponentText.DepartureDateSelector.time)}
-            </label>
+      <AnimatePresence initial={false}>
+        {selectedOption.type !== DepartureDateState.Now && (
+          <motion.div
+            className={style.dateAndTimeSelectorsWrapper}
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: 'auto' },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ easings: ['easeInOut'] }}
+          >
+            <div className={style.dateAndTimeSelectors}>
+              <div className={style.dateSelector}>
+                <label htmlFor="departureDateSelector">
+                  {t(ComponentText.DepartureDateSelector.date)}
+                </label>
+                <input
+                  type="date"
+                  id="departureDateSelector"
+                  value={selectedDate.toISOString().slice(0, 10)}
+                  onChange={internalOnDateChange}
+                />
+              </div>
+              <div className={style.timeSelector}>
+                <label htmlFor="departureTimeSelector">
+                  {t(ComponentText.DepartureDateSelector.time)}
+                </label>
 
-            <input
-              type="time"
-              id="departureTimeSelector"
-              value={selectedTime}
-              onChange={internalOnTimeChange}
-            />
-          </div>
-        </div>
-      )}
+                <input
+                  type="time"
+                  id="departureTimeSelector"
+                  value={selectedTime}
+                  onChange={internalOnTimeChange}
+                />
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
