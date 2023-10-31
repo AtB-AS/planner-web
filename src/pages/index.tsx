4f99cb0f2b5e8c6e799c1350b9e56bea945b8864
Mikael Brevik <mikaelbre@gmail.com>
2023-10-31 13:07:34 +0100

    feat: set assistant as front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,4 @@
-import DefaultLayout from '@atb/layouts/default';
-import { WithGlobalData, withGlobalData } from '@atb/layouts/global-data';
-import { CommonText, useTranslation } from '@atb/translations';
-import type { NextPage } from 'next';
+import AssistantPage, { getServerSideProps } from './assistant';
 
-function IndexContent() {
-  const { t } = useTranslation();
-  return <h1>{t(CommonText.Titles.siteTitle)}</h1>;
-}
-
-type IndexProps = WithGlobalData<{}>;
-const Index: NextPage<IndexProps> = (props) => {
-  return (
-    <DefaultLayout {...props}>
-      <IndexContent />
-    </DefaultLayout>
-  );
-};
-
-export default Index;
-
-export const getServerSideProps = withGlobalData();
+export default AssistantPage;
+export { getServerSideProps };
