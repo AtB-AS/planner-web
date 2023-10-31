cd241c7c41560f4ad7b58b168a3b766f4ede07f0
mortennordseth <morten@nordseth.net>
2023-10-31 08:45:59 +0100

    refactor: reduce nesting with early exists

--- src/pages/api/assistant/trip.ts
+++ src/pages/api/assistant/trip.ts
@@ -9,6 +9,7 @@ import { constants } from 'http2';
 export default handlerWithAssistantClient<TripApiReturnType>({
   async GET(req, res, { client, ok }) {
     const tripQuery = parseTripQuery(req.query);
+
     if (!tripQuery) {
       return errorResultAsJson(
         res,
@@ -16,50 +17,44 @@ export default handlerWithAssistantClient<TripApiReturnType>({
         ServerText.Endpoints.invalidData,
       );
     }
-      const from = await client.reverse(
-        parseFloat(tripQuery.fromLat),
-        parseFloat(tripQuery.fromLon),
-        tripQuery.fromLayer,
-      );
-      const to = await client.reverse(
-        parseFloat(tripQuery.toLat),
-        parseFloat(tripQuery.toLon),
-        tripQuery.toLayer,
-      );
-      const transportModeFilter = parseFilterQuery(tripQuery.filter);
 
-      const arriveBy = 'arriveBy' in tripQuery;
-      const departureDate = arriveBy
-        ? new Date(Number(tripQuery.arriveBy))
-        : new Date(Number(tripQuery.departBy)) || new Date();
+    const from = await client.reverse(
+      parseFloat(tripQuery.fromLat),
+      parseFloat(tripQuery.fromLon),
+      tripQuery.fromLayer,
+    );
+    const to = await client.reverse(
+      parseFloat(tripQuery.toLat),
+      parseFloat(tripQuery.toLon),
+      tripQuery.toLayer,
+    );
+    const transportModeFilter = parseFilterQuery(tripQuery.filter);
 
-      if (from && to) {
-        return tryResult(req, res, async () => {
-          return ok(
-            await client.trip({
-              from,
-              to,
-              ...(arriveBy
-                ? { arriveBy: departureDate }
-                : { departBy: departureDate }),
-              transportModes: transportModeFilter,
-              cursor: tripQuery.cursor,
-            }),
-          );
-        });
-      } else {
-        return errorResultAsJson(
-          res,
-          constants.HTTP_STATUS_BAD_REQUEST,
-          ServerText.Endpoints.invalidData,
-        );
-      }
-    } else {
+    const arriveBy = 'arriveBy' in tripQuery;
+    const departureDate = arriveBy
+      ? new Date(Number(tripQuery.arriveBy))
+      : new Date(Number(tripQuery.departBy)) || new Date();
+
+    if (!from || !to) {
       return errorResultAsJson(
         res,
         constants.HTTP_STATUS_BAD_REQUEST,
         ServerText.Endpoints.invalidData,
       );
     }
+
+    return tryResult(req, res, async () => {
+      return ok(
+        await client.trip({
+          from,
+          to,
+          ...(arriveBy
+            ? { arriveBy: departureDate }
+            : { departBy: departureDate }),
+          transportModes: transportModeFilter,
+          cursor: tripQuery.cursor,
+        }),
+      );
+    });
   },
 });
