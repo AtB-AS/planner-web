120e260e2fc196245fdd70cdefbb50d406b6b4ad
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-11-06 14:52:10 +0100

    fix: use useEffect instead of useLayoutEffect

--- src/utils/use-client-width.ts
+++ src/utils/use-client-width.ts
@@ -1,11 +1,11 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useResizeObserver from '@react-hook/resize-observer';
 
 export function useClientWidth<T extends HTMLElement>() {
   const ref = useRef<T | null>(null);
   const [width, setWidth] = useState(0);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (ref.current) setWidth(ref.current.clientWidth);
   }, [ref]);
 
