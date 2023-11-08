815c81d55cb912c6d9facb06b1be2429a4c9d178
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-11-08 22:04:50 +0100

    refactor: non-alias imports

--- src/page-modules/assistant/index.ts
+++ src/page-modules/assistant/index.ts
@@ -1,9 +1,4 @@
 export * from './types';
-export {
-  parseTripQuery,
-  createTripQuery,
-  departureDateToDepartureMode,
-  departureModeToDepartureDate,
-} from './utils';
+export { parseTripQuery, createTripQuery } from './utils';
 export type { AssistantLayoutProps } from './layout';
 export { default as AssistantLayout } from './layout';
