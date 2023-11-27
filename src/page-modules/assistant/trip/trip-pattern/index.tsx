89f590b7bd572ac334994041ebb8574835527dad
Morten Nordseth <43166974+mortennordseth@users.noreply.github.com>
2023-11-27 09:18:34 +0100

    refactor: remove old comment
    Co-authored-by: Mikael Brevik <mikaelbre@gmail.com>

--- src/page-modules/assistant/trip/trip-pattern/index.tsx
+++ src/page-modules/assistant/trip/trip-pattern/index.tsx
@@ -53,7 +53,7 @@ export default function TripPattern({
 
   return (
     <motion.a
-      href={`/assistant/${tripPattern.compressedQuery}`} // TODO: Use correct href.
+      href={`/assistant/${tripPattern.compressedQuery}`}
       className={style.tripPattern}
       initial={{ opacity: 0, x: -10 }}
       animate={{ opacity: 1, x: 0 }}
