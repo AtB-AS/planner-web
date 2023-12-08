ec8051379f61ade4b1aee83ad421d3421c1503ec
Morten Nordseth <43166974+mortennordseth@users.noreply.github.com>
2023-12-08 14:06:45 +0100

    feat: sort quays (#122)
    * feat: sort quays
    
    * refactor: sort function
    
    Co-authored-by: Mikael Brevik <mikaelbre@gmail.com>
    
    * fix: sorting function
    
    * test: quay sorting
    
    ---------
    
    Co-authored-by: Mikael Brevik <mikaelbre@gmail.com>

--- src/page-modules/departures/__tests__/departure.test.tsx
+++ src/page-modules/departures/__tests__/departure.test.tsx
@@ -12,6 +12,7 @@ import userEvent from '@testing-library/user-event';
 import { NearestStopPlaces } from '..';
 
 import { GeocoderApi } from '@atb/page-modules/departures/server/geocoder';
+import { sortQuays } from '../server/journey-planner/utils';
 
 afterEach(function () {
   cleanup();
@@ -142,5 +143,49 @@ describe('departure page', function () {
     expect(
       output.getByText('Finner ingen holdeplasser i nærheten'),
     ).toBeInTheDocument();
+  });
+
+  it('should sort quays by public code and departures', () => {
+    const quays = [
+      {
+        ...departureDataFixture.quays[0],
+        publicCode: 'B2',
+      },
+      {
+        ...departureDataFixture.quays[0],
+        publicCode: 'B1',
+      },
+      {
+        ...departureDataFixture.quays[0],
+        publicCode: null,
+      },
+      {
+        ...departureDataFixture.quays[0],
+        publicCode: '1',
+      },
+      {
+        ...departureDataFixture.quays[0],
+        publicCode: '2',
+        departures: [],
+      },
+      {
+        ...departureDataFixture.quays[0],
+        publicCode: 'A1',
+      },
+      {
+        ...departureDataFixture.quays[0],
+        publicCode: '0',
+      },
+    ];
+
+    quays.sort(sortQuays);
+
+    expect(quays[0].publicCode).toBe('0');
+    expect(quays[1].publicCode).toBe('1');
+    expect(quays[2].publicCode).toBe('A1');
+    expect(quays[3].publicCode).toBe('B1');
+    expect(quays[4].publicCode).toBe('B2');
+    expect(quays[5].publicCode).toBe(null);
+    expect(quays[6].publicCode).toBe('2');
   });
 });
