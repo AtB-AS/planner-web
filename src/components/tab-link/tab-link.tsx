aabb859407ae39603978d5457ee7648d1ea5c7ac
Jonas Brunvoll Larsson <jbl@variant.no>
2023-12-11 15:12:17 +0100

    Added the indicator '/' to set the 'assistant' as the default initial active mode on page load.

--- src/components/tab-link/tab-link.tsx
+++ src/components/tab-link/tab-link.tsx
@@ -20,7 +20,7 @@ type LinkProps = {
 const links: LinkProps[] = [
   {
     title: ComponentText.TabLink.assistant,
-    href: '/assistant',
+    href: '/' || '/assistant',
   },
   {
     title: ComponentText.TabLink.departures,
