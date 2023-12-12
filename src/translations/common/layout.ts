fca870bab0c1c0cc3d5d9a64c4a8e2f96cb3cf1a
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-12-12 16:03:08 +0100

    fix: update text for FRAM

--- src/translations/common/layout.ts
+++ src/translations/common/layout.ts
@@ -15,18 +15,18 @@ export const Layout = orgSpecificTranslations(LayoutInternal, {
   nfk: {
     meta: {
       defaultDescription: _(
-        'Finn rutetider, holdeplasser og tilbud for buss, trikk, båt og tog i Nordland med reiseplanleggeren.',
-        'Find timetables, stops and offers for bus, tram, boat and train in Nordland with the travel planner.',
-        'Finn rutetider, haldeplassar og tilbod for buss, trikk, båt og tog i Nordland med reiseplanleggjaren.',
+        'Finn rutetider, holdeplasser og tilbud for buss, båt og tog i Nordland med reiseplanleggeren.',
+        'Find timetables, stops and offers for bus, boat and train in Nordland with the travel planner.',
+        'Finn rutetider, haldeplassar og tilbod for buss, båt og tog i Nordland med reiseplanleggjaren.',
       ),
     },
   },
   fram: {
     meta: {
       defaultDescription: _(
-        'Finn rutetider, holdeplasser og tilbud for buss, trikk, båt og tog i Møre og Romsdal med reiseplanleggeren.',
-        'Find timetables, stops and offers for bus, tram, boat and train in Møre og Romsdal with the travel planner.',
-        'Finn rutetider, haldeplassar og tilbod for buss, trikk, båt og tog i Møre og Romsdal med reiseplanleggjaren.',
+        'Finn rutetider, holdeplasser, kaier og tilbud for buss, hurtigbåt og ferge i Møre og Romsdal med reiseplanleggeren.',
+        'Find timetables, stops and offers for bus, boat and ferry in Møre og Romsdal with the travel planner.',
+        'Finn rutetider, haldeplassar, kaier og tilbod for buss, hurtigbåt og ferje i Møre og Romsdal med reiseplanleggjaren.',
       ),
     },
   },
