d32a25bdf52b413036e65b6b540784709298c57b
Jonas Brunvoll Larsson <jbl@variant.no>
2023-12-13 15:51:51 +0100

    Reformulate comment.

--- src/modules/search-time/selector/index.tsx
+++ src/modules/search-time/selector/index.tsx
@@ -68,7 +68,7 @@ export default function SearchTimeSelector({
   const isPastDate = (selectedDate: string) => {
     const today = new Date().toISOString().split('T')[0];
 
-    // example: If reselecting  from 2024 to 2023, the date will autmatically reset to the current date.
+    // If reselecting year from year > current year to current year, the date will autmatically be reset to the current date.
     if (selectedDate.substring(0, 4) < initialDate.getFullYear().toString()) {
       resetToCurrentDate();
     }
