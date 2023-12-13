a35477dec63df6af97a852e66e168f24f1972ae1
Jonas Brunvoll Larsson <59939294+jonasbrunvoll@users.noreply.github.com>
2023-12-13 09:12:48 +0100

    feat: component to toggle between the assistant and departures pages.  (#123)
    * Added toggle component to assistant and departures.
    
    * Remove excessive css - styling.
    
    * Remove unsused HeaderLinks from menu.
    
    * Fixed line formating and removed comments.
    
    * Removed .vscode/ from .gitignore
    
    * Added blank lines back again.
    
    * Moving linktitle into component text + renaming.
    
    * Improvment to setInitialActiveLink to make it more generic.
    
    * Remove surronding string.
    
    * Remove exessive css.
    
    * Rename component from to 'AssistantDepartureToggle to ''
    
    * Rename from 'link' to 'href'.
    
    * Remove exessive function.
    
    * Removed optinal prop href from compoent. Removed exessive code from component to simplify logic.
    
    * Added logic to rewrites path if dynamic. Neccessary to pass tests,  as dynami hrefs are by default not accepted by <Link/>.
    
    * Rewrote component to reciev the URL from a parant component as a prop. The active link is set based on this prop, thus simplefied the component logic.
    
    * Rewrite from let to const
    
    * Remove exessive code.
    
    * Remove exessive code to simplify logic.
    
    * Added the indicator '/' to set the 'assistant' as the default initial active mode on page load.
    
    * Added regex expression to set the indicator, used to set assistant as the standard default active mode.
    
    * Removed comment.
    
    * Removed regex logic from code. This solution seems fine as it sets the correct active path for '/', '/assistant' and 'departures'.
    
    * Added type to ensure that only valid routs can be passed as attributes in <TabLink/>. With this approach, the problem of having to distingush between if  either '/'  or '/assistant' was passed in as a prop is avoided.
    
    * Renamed LinkProps -> TabLink.
    
    * Update src/page-modules/assistant/layout.tsx
    
    Co-authored-by: Mikael Brevik <mikaelbre@gmail.com>
    
    * Update src/page-modules/departures/layout.tsx
    
    Co-authored-by: Mikael Brevik <mikaelbre@gmail.com>
    
    ---------
    
    Co-authored-by: Mikael Brevik <mikaelbre@gmail.com>

---
+++ src/components/tab-link/tab-link.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+import style from './tab-link.module.css';
+import {
+  ComponentText,
+  TranslatedString,
+  useTranslation,
+} from '@atb/translations';
+import { Typo } from '../typography';
+import { AnimatePresence, motion } from 'framer-motion';
+
+type TabLinkProps = {
+  activePath: Route;
+};
+
+type TabLink = {
+  title: TranslatedString;
+  route: Route;
+};
+
+type Route = '/assistant' | '/departures';
+
+const tabLinks: TabLink[] = [
+  {
+    title: ComponentText.TabLink.assistant,
+    route: '/assistant',
+  },
+  {
+    title: ComponentText.TabLink.departures,
+    route: '/departures',
+  },
+];
+
+const TabLink = ({ activePath }: TabLinkProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={style.container}>
+      {tabLinks.map((tabLink, index) => {
+        const isActive = tabLink.route === activePath;
+        return (
+          <Link key={index} className={style.href} href={tabLink.route}>
+            <Typo.p
+              textType={isActive ? 'body__primary--bold' : 'body__primary'}
+            >
+              {t(tabLink.title)}
+            </Typo.p>
+            <AnimatePresence initial={false}>
+              {isActive && (
+                <motion.div
+                  className={style.underline}
+                  layoutId="underline"
+                  initial="collapsed"
+                  animate="open"
+                  exit="collapsed"
+                  transition={{ duration: 0.25 }}
+                />
+              )}
+            </AnimatePresence>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+export default TabLink;
