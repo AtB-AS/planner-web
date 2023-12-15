a1b3eec6178b89918aaf958b229501f04fed6b10
Morten Nordseth <43166974+mortennordseth@users.noreply.github.com>
2023-12-15 14:55:55 +0100

    fix: quay sorting (#154)

--- src/page-modules/departures/server/journey-planner/utils.ts
+++ src/page-modules/departures/server/journey-planner/utils.ts
@@ -2,8 +2,12 @@ import { Quay } from './validators';
 
 export function sortQuays(a: Quay, b: Quay) {
   // Place quays with no departures at the end
-  if (!a.publicCode || a.departures.length === 0) return 1;
-  if (!b.publicCode || b.departures.length === 0) return -1;
+  if (a.departures.length === 0) return 1;
+  if (b.departures.length === 0) return -1;
+
+  // Sort by public code
+  if (!a.publicCode) return 1;
+  if (!b.publicCode) return -1;
 
   const publicA = parseInt(a.publicCode, 10);
   const publicB = parseInt(b.publicCode, 10);
