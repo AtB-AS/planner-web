dca200fa14cfc794702cb9d21dd9e4a012a85cd6
Jonas Brunvoll Larsson <jbl@variant.no>
2023-12-15 14:01:23 +0100

    Added style to match App solution. Need some code refactoring before being pushed.

--- src/page-modules/departures/stop-place/index.tsx
+++ src/page-modules/departures/stop-place/index.tsx
@@ -134,7 +134,6 @@ export function EstimatedCallList({ quay }: EstimatedCallListProps) {
             </Typo.span>
           )}
         </div>
-
         <MonoIcon
           icon={isCollapsed ? 'navigation/ExpandMore' : 'navigation/ExpandLess'}
         />
@@ -204,7 +203,7 @@ export function EstimatedCallItem({
         <div className={style.transportInfo}>
           {(departure.transportMode || departure.publicCode) && (
             <>
-              {departure.cancelled ? (
+              {!departure.cancelled ? (
                 <ColorIcon
                   icon="status/Error"
                   className={style.situationIcon}
@@ -225,7 +224,14 @@ export function EstimatedCallItem({
             </>
           )}
 
-          <p>{departure.name}</p>
+          <Typo.p
+            className={!departure.cancelled ? style.textColor__secondary : ''}
+            textType={
+              !departure.cancelled ? 'body__primary--strike' : 'body__primary'
+            }
+          >
+            {departure.name}
+          </Typo.p>
         </div>
 
         <DepartureTime
@@ -252,8 +258,8 @@ export function DepartureTime({
   return (
     <div className={style.departureTime}>
       <Typo.p
-        textType="body__primary--bold"
-        className={cancelled ? style.departureTime__cancelled : ''}
+        className={!cancelled ? style.textColor__secondary : ''}
+        textType={!cancelled ? 'body__primary--strike' : 'body__primary'}
         aria-label={
           cancelled
             ? `${screenReaderPause} ${t(
