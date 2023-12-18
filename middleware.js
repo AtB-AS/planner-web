4877b0723957101909cdcf372dc3c4bb47231c6c
Mikael Brevik <mikaelbre@gmail.com>
2023-12-18 12:17:51 +0100

    temp: fixes cors for temp

---
+++ middleware.js
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://frammr.no',
+  'https://reise.frammr.no',
+];
+
+export function middleware(req) {
+  // retrieve the current response
+  const res = NextResponse.next();
+
+  req.headers.get('origin');
+
+  if (allowedOrigins.includes(origin)) {
+    res.headers.append('Access-Control-Allow-Origin', origin);
+  }
+
+  res.headers.append('Access-Control-Allow-Credentials', 'true');
+  res.headers.append(
+    'Access-Control-Allow-Methods',
+    'GET,DELETE,PATCH,POST,PUT',
+  );
+  res.headers.append(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  );
+
+  return res;
+}
+
+// specify the path regex to apply the middleware to
+export const config = {
+  matcher: '/api/departures/:path*',
+};
