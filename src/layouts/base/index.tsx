5cea2f548eb346e85980c0a62d178613ab647712
Jonas Brunvoll Larsson <jbl@variant.no>
2023-12-18 13:39:32 +0100

    Remove flag from meta tag.

--- src/layouts/base/index.tsx
+++ src/layouts/base/index.tsx
@@ -36,7 +36,7 @@ export function BaseLayout({ children, title }: BaseLayoutProps) {
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
         />
       </Head>
 
