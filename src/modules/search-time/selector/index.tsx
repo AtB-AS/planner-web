f14faef1e528bad3cffd8b4633ce343922b1a6fb
Jonas Brunvoll Larsson <jbl@variant.no>
2023-12-19 16:58:54 +0100

    Undo last commit.

--- src/modules/search-time/selector/index.tsx
+++ src/modules/search-time/selector/index.tsx
@@ -29,14 +29,6 @@ export default function SearchTimeSelector({
     format(initialDate, 'HH:mm'),
   );
 
-  const resetToCurrentTime = () => {
-    setSelectedTime(() => format(new Date(), 'HH:mm'));
-  };
-
-  const resetToCurrentDate = () => {
-    setSelectedDate(new Date());
-  };
-
   const internalOnStateChange = (state: SearchMode) => {
     const newState =
       state === 'now'
@@ -50,10 +42,16 @@ export default function SearchTimeSelector({
             ).getTime(),
           };
 
-    resetToCurrentDate();
-    resetToCurrentTime();
     setSelectedMode(newState);
     onChange(newState);
+  };
+
+  const resetToCurrentTime = () => {
+    setSelectedTime(() => format(new Date(), 'HH:mm'));
+  };
+
+  const resetToCurrentDate = () => {
+    setSelectedDate(new Date());
   };
 
   const isPastDate = (selectedDate: string) => {
