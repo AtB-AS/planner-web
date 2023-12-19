9026eb929fbac14148a4728b275e7c3289c23256
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-12-19 11:31:48 +0100

    fix: fix nynorsk spelling (#177)

--- src/translations/components/tab-link.ts
+++ src/translations/components/tab-link.ts
@@ -2,5 +2,5 @@ import { translation as _ } from '@atb/translations/commons';
 
 export const TabLink = {
   assistant: _('Reiseplanlegger', 'Travel planner', 'Reiseplanleggar'),
-  departures: _('Avganger', 'Departures', 'Avganger'),
+  departures: _('Avganger', 'Departures', 'Avgangar'),
 };
