a7ecba8d106da2b101d62d31e6429a90e2173253
Mikael Brevik <mikaelbre@gmail.com>
2024-02-06 10:58:07 +0100

    improves test

--- e2e-tests/search-test.spec.ts
+++ e2e-tests/search-test.spec.ts
@@ -4,22 +4,26 @@ test('Test fetching Kristiansund - Molde and loading more after first result', a
   page,
 }) => {
   await page.goto(process.env.E2E_URL ?? 'http://localhost:3000');
+
   await page.getByRole('textbox', { name: 'From' }).click();
   await page.getByRole('textbox', { name: 'From' }).fill('Kristiansund');
   await page.getByRole('option', { name: 'Kristiansund Kristiansund' }).click();
-  await page.getByRole('textbox', { name: 'To' }).click();
-  await page.getByRole('textbox', { name: 'To' }).click();
 
+  await page.getByRole('textbox', { name: 'To' }).click();
   await page.getByRole('textbox', { name: 'To' }).fill('Molde');
   await page.getByRole('option', { name: 'Molde Molde', exact: true }).click();
-  await page.getByRole('button', { name: 'More choices' }).click();
 
+  await page.getByRole('button', { name: 'More choices' }).click();
   await page.getByText('Bus', { exact: true }).click();
   await page.getByRole('button', { name: 'Find departures' }).click();
 
-  const orderSent = page.getByTestId('trip-pattern-0');
+  const tripPatternItem = page.getByTestId('trip-pattern-0');
+  await tripPatternItem.waitFor();
 
-  await orderSent.waitFor();
-
+  const additionalRequest = page.waitForRequest((request) => {
+    return request.url().includes('trip') && request.url().includes('cursor');
+  });
   await page.getByRole('button', { name: 'Load more results' }).click();
+
+  await additionalRequest;
 });
