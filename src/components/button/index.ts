de3c8b1d4e5b4d2e46418a1fdf0352dc3cdb4632
Jonas Brunvoll Larsson <jbl@variant.no>
2024-02-13 11:56:18 +0100

    Remove exessive line.

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,5 +1,4 @@
 export { default as ButtonLink } from './link';
 export type { ButtonLinkProps } from './link';
-
 export { default as Button } from './button';
 export type { ButtonProps } from './button';
