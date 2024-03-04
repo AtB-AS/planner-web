e8b427e29d1ecf34bb2764f4c695c0e753c08e4a
mortennordseth <morten@nordseth.net>
2024-03-04 08:54:12 +0100

    fix: add allow url for CORS reisnordland com

--- src/pages/api/departures/autocomplete.ts
+++ src/pages/api/departures/autocomplete.ts
@@ -46,5 +46,7 @@ export default handlerWithDepartureClient<AutocompleteApiReturnType>(
     /\.frammr.no$/,
     'https://reisnordland.no',
     /\.reisnordland.no$/,
+    'https://reisnordland.com',
+    /\.reisnordland.com$/,
   ],
 );
