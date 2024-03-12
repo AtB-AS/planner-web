c98b8e95dff954c94139bd196e0ba7ea6a765e9f
Mikael Brevik <mikaelbre@gmail.com>
2024-03-12 15:01:12 +0100

    fix: adds validation to line filter

--- src/components/labled-input/index.tsx
+++ src/components/labled-input/index.tsx
@@ -1,21 +1,36 @@
+import { andIf } from '@atb/utils/css';
 import { ChangeEvent } from 'react';
+import { MessageBox } from '../message-box';
 import style from './labled-input.module.css';
 
-type LabeledInput = {
+export type LabeledInputProps = {
   label: string;
   placeholder: string;
   value: string;
+  validation?: {
+    message: string;
+    status: 'error' | 'warning';
+  };
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
-};
+} & JSX.IntrinsicElements['input'];
 
 export default function LabeledInput({
   label,
   placeholder,
   value,
   onChange,
-}: LabeledInput) {
+  ...props
+}: LabeledInputProps) {
+  const status = props.validation?.status;
+
   return (
-    <div className={style.container}>
+    <div
+      className={andIf({
+        [style.container]: true,
+        [style['container--error']]: status === 'error',
+        [style['container--warning']]: status === 'warning',
+      })}
+    >
       <label className={style.label}>{label}</label>
       <input
         className={style.input}
@@ -23,7 +38,16 @@ export default function LabeledInput({
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        {...props}
       />
+
+      {props.validation && (
+        <MessageBox
+          message={props.validation.message}
+          type={status!}
+          borderRadius={false}
+        />
+      )}
     </div>
   );
 }
