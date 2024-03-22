ea34f646141619366668fafb3ea2d2340dd06259
Jonas Brunvoll Larsson <jbl@variant.no>
2024-03-22 14:50:43 +0100

    Add authorities that should be possible to filter on.

--- src/pages/api/assistant/lines/[authorityId].ts
+++ src/pages/api/assistant/lines/[authorityId].ts
@@ -2,10 +2,24 @@ import { tryResult } from '@atb/modules/api-server';
 import { LinesApiReturnType } from '@atb/page-modules/assistant/client';
 import { handlerWithAssistantClient } from '@atb/page-modules/assistant/server';
 
+/*
+ * Todo:
+ *  1. Make sure that all authorities that should be possible to filter on are added.
+ *  2. Should the 'authorities' be set somewhere else?
+ */
+const authorities: string[] = [
+  'NSB:Authority:NSB',
+  'SJN:Authority:SJN',
+  'UNI:Authority:UNI',
+  'GFS:Authority:1',
+  'VYG:Authority:TAG',
+  'BSR:Authority:1',
+  'KOL:Authority:30',
+];
+
 export default handlerWithAssistantClient<LinesApiReturnType>({
   async GET(req, res, { client, ok }) {
     const { authorityId } = req.query;
-    const authorities: string[] = [];
 
     if (typeof authorityId === 'string') {
       authorities.push(authorityId);
