2acd6bc2c933da6735fd1a75968fc87b105a408f
mortennordseth <morten@nordseth.net>
2024-06-05 15:49:17 +0200

    refactor: parse global messages using zod

--- src/modules/global-messages/types.ts
+++ src/modules/global-messages/types.ts
@@ -1,20 +1,28 @@
-import { MessageMode } from '@atb/components/message-box';
-import { LocalizedString } from '@atb/translations/commons';
-import { Timestamp } from 'firebase/firestore';
+import { languageAndTextSchema } from '@atb/translations/types';
+import { z } from 'zod';
+
+const messageModeSchema = z.union([
+  z.literal('info'),
+  z.literal('valid'),
+  z.literal('warning'),
+  z.literal('error'),
+]);
 
 export enum GlobalMessageContextEnum {
   plannerWebAssistant = 'planner-web-assistant',
 }
 
-export type GlobalMessageType = {
-  id: string;
-  active: boolean;
-  title?: LocalizedString[];
-  body: LocalizedString[];
-  type: MessageMode;
-  subtle?: boolean;
-  context: GlobalMessageContextEnum[];
-  isDismissable?: boolean;
-  startDate?: Timestamp;
-  endDate?: Timestamp;
-};
+export const globalMessageTypeSchema = z.object({
+  id: z.string(),
+  active: z.boolean(),
+  title: z.array(languageAndTextSchema).optional(),
+  body: z.array(languageAndTextSchema),
+  type: messageModeSchema,
+  subtle: z.boolean().optional(),
+  context: z.array(z.nativeEnum(GlobalMessageContextEnum)),
+  isDismissable: z.boolean().optional(),
+  startDate: z.date().optional(),
+  endDate: z.date().optional(),
+});
+
+export type GlobalMessageType = z.infer<typeof globalMessageTypeSchema>;
