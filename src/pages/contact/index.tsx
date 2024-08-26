6d259e3b7d1daaf08745c3cbfef6264e0f38b496
Jonas Brunvoll Larsson <jbl@variant.no>
2024-08-26 10:23:21 +0200

    Add 404 page if CONTACT_PAGE is undefined.

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,6 +1,7 @@
 import DefaultLayout from '@atb/layouts/default';
 import { withGlobalData, WithGlobalData } from '@atb/layouts/global-data';
 import { ContactLayout, ContactLayoutProps } from '@atb/page-modules/contact';
+import { ErrorContent } from '@atb/page-modules/error';
 import { NextPage } from 'next';
 
 export type ContactContentProps = { title: string };
@@ -10,11 +11,16 @@ export type ContactPageProps = WithGlobalData<
 >;
 
 function ContactContent(props: ContactContentProps) {
-  return (
-    <div>
-      <h1>Initial title contact schema</h1>
-    </div>
-  );
+  const contactPage = process.env.NEXT_PUBLIC_CONTACT_PAGE;
+
+  if (contactPage) {
+    return (
+      <section>
+        <h1>Initial title contact schema</h1>
+      </section>
+    );
+  }
+  return <ErrorContent statusCode={404} />;
 }
 
 const ContactPage: NextPage<ContactPageProps> = (props) => {
