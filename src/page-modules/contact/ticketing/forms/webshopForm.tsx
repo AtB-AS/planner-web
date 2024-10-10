74b0772514c23fa4231e5cba86a05668a562cd74
Jonas Brunvoll Larsson <jbl@variant.no>
2024-10-10 16:33:38 +0200

    Add ticketing webshop form

---
+++ src/page-modules/contact/ticketing/forms/webshopForm.tsx
@@ -0,0 +1,158 @@
+import { SectionCard } from '../../components/section-card';
+import { PageText, useTranslation } from '@atb/translations';
+import { ticketingFormEvents } from '../events';
+import { TicketingContextType } from '../ticketingStateMachine';
+import { Typo } from '@atb/components/typography';
+import { Textarea } from '../../components/input/textarea';
+import { FileInput } from '../../components/input/file';
+import { Input } from '../../components/input';
+
+type WebshopFormProps = {
+  state: { context: TicketingContextType };
+  send: (event: typeof ticketingFormEvents) => void;
+};
+
+export const WebshopForm = ({ state, send }: WebshopFormProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <SectionCard title={t(PageText.Contact.ticketing.webshop.description)}>
+        <Typo.p textType="body__primary">
+          {t(PageText.Contact.input.orderId.info)}
+        </Typo.p>
+        <Input
+          label={PageText.Contact.input.orderId.label}
+          type="text"
+          autoComplete="given-name additional-name"
+          name="orderId"
+          value={state.context.orderId || ''}
+          errorMessage={state.context?.errorMessages['orderId']?.[0]}
+          onChange={(e) =>
+            send({
+              type: 'ON_INPUT_CHANGE',
+              inputName: 'orderId',
+              value: e.target.value,
+            })
+          }
+        />
+      </SectionCard>
+
+      <SectionCard title={t(PageText.Contact.input.question.title)}>
+        <Typo.p textType="body__primary">
+          {t(PageText.Contact.input.question.info)}
+        </Typo.p>
+        <Textarea
+          value={state.context.question || ''}
+          onChange={(e) =>
+            send({
+              type: 'ON_INPUT_CHANGE',
+              inputName: 'question',
+              value: e.target.value,
+            })
+          }
+          error={
+            state.context.errorMessages['question']?.[0]
+              ? t(state.context.errorMessages['question']?.[0]).toString()
+              : undefined
+          }
+        />
+        <FileInput
+          name="attachments"
+          label={t(PageText.Contact.input.question.attachment)}
+          onChange={(files) => {
+            send({
+              type: 'ON_INPUT_CHANGE',
+              inputName: 'attachments',
+              value: files,
+            });
+          }}
+        />
+      </SectionCard>
+      <SectionCard title={t(PageText.Contact.aboutYouInfo.title)}>
+        {/*
+         Must be merged first: https://github.com/AtB-AS/planner-web/pull/387
+        <Input
+          label={PageText.Contact.input.customerNumber.labelOptional}
+          type="text"
+          name="customerNumber"
+          value={state.context.customerNumber || ''}
+          errorMessage={state.context?.errorMessages['customerNumber']?.[0]}
+          onChange={(e) =>
+            send({
+              type: 'ON_INPUT_CHANGE',
+              inputName: 'customerNumber',
+              value: e.target.value,
+            })
+          }
+        />
+        */}
+        <Input
+          label={PageText.Contact.input.firstName.label}
+          type="text"
+          autoComplete="given-name additional-name"
+          name="firstName"
+          value={state.context.firstName || ''}
+          onChange={(e) =>
+            send({
+              type: 'ON_INPUT_CHANGE',
+              inputName: 'firstName',
+              value: e.target.value,
+            })
+          }
+        />
+
+        <Input
+          label={PageText.Contact.input.lastName.label}
+          type="text"
+          autoComplete="family-name"
+          name="lastName"
+          value={state.context.lastName || ''}
+          onChange={(e) =>
+            send({
+              type: 'ON_INPUT_CHANGE',
+              inputName: 'lastName',
+              value: e.target.value,
+            })
+          }
+        />
+        <Input
+          label={PageText.Contact.input.email.label}
+          type="email"
+          autoComplete="email"
+          name="email"
+          value={state.context.email || ''}
+          errorMessage={
+            state.context.customerNumber
+              ? undefined
+              : state.context?.errorMessages['email']?.[0]
+          }
+          onChange={(e) =>
+            send({
+              type: 'ON_INPUT_CHANGE',
+              inputName: 'email',
+              value: e.target.value,
+            })
+          }
+        />
+        <Input
+          label={PageText.Contact.input.phoneNumber.label}
+          type="phoneNumber"
+          autoComplete="tel"
+          name="phoneNumber"
+          value={state.context.phoneNumber || ''}
+          errorMessage={state.context?.errorMessages['phoneNumber']?.[0]}
+          onChange={(e) =>
+            send({
+              type: 'ON_INPUT_CHANGE',
+              inputName: 'phoneNumber',
+              value: e.target.value,
+            })
+          }
+        />
+      </SectionCard>
+    </div>
+  );
+};
+
+export default WebshopForm;
