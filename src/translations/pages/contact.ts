7af6b2b0588891b8bcb54bb7ee8d6dad3676a9a9
Jonas Brunvoll Larsson <jbl@variant.no>
2024-10-18 13:33:06 +0200

    ticketing webshop form

--- src/translations/pages/contact.ts
+++ src/translations/pages/contact.ts
@@ -940,6 +940,11 @@ export const Contact = {
         'First name and middle name',
         'Fornamn og mellomnamn',
       ),
+      labelOptional: _(
+        'Fornavn og mellomnavn (valgfritt)',
+        'First name and middle name (optinal)',
+        'Fornamn og mellomnamn (valfritt)',
+      ),
       errorMessages: {
         empty: _(
           'Vennligst fyll ut informasjon om deg',
@@ -951,6 +956,11 @@ export const Contact = {
 
     lastName: {
       label: _('Etternavn', 'Lastname', 'Etternamn'),
+      labelOptional: _(
+        'Etternavn (valgfritt)',
+        'Lastname (optional)',
+        'Etternamn (valfritt)',
+      ),
       errorMessages: {
         empty: _(
           'Vennligst fyll ut informasjon om deg',
@@ -962,6 +972,12 @@ export const Contact = {
 
     email: {
       label: _('E-post', 'Email', 'E-post'),
+      labelOptionalIfCustomerNumberIsProvided: _(
+        'E-post (valgfritt hvis kundenummer er oppgitt)',
+        'Email (optional if customber number is provided)',
+        'E-post (valfritt viss kundenummer er oppgitt) ',
+      ),
+
       isResponseWanted: {
         label: _(
           'E-post (må fylles ut hvis du vil ha svar)',
@@ -1018,6 +1034,11 @@ export const Contact = {
 
     phoneNumber: {
       label: _('Telefonnummer', 'Phonenumber', 'Telefonnummer'),
+      labelOptional: _(
+        'Telefonnummer (valgfritt)',
+        'Phonenumber (optinal)',
+        'Telefonnummer (valfritt)',
+      ),
       errorMessages: {
         empty: _(
           'Vennligst fyll ut informasjon om deg',
@@ -1384,7 +1405,7 @@ export const Contact = {
         empty: _('Område mangler', 'Area is missing', 'Område mangler'),
       },
     },
-    
+
     appPhoneNumber: {
       label: _(
         'Registrert mobilnummer',
@@ -1421,7 +1442,7 @@ export const Contact = {
         ),
       },
     },
-    
+
     question: {
       title: _(
         'Hva ønsker du å spørre om?',
@@ -1518,9 +1539,10 @@ export const Contact = {
         empty: _(
           'Grunn for refusjon mangler',
           'Reason for refund is missing',
-          'Grunn for refusjon mangler',)
-      }
-    }
+          'Grunn for refusjon mangler',
+        ),
+      },
+    },
   },
   success: {
     backButton: _(
