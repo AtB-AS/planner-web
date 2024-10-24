f61c41f67f048668d8335e16ca9ed8eb1a8f67c4
Jonas Brunvoll Larsson <jbl@variant.no>
2024-10-24 14:57:46 +0200

    Fix translation error

--- src/translations/pages/contact.ts
+++ src/translations/pages/contact.ts
@@ -271,7 +271,7 @@ export const Contact = {
 
       aboutYourTrip: {
         title: _(
-          'Om den planlagte reisa di',
+          'Om den planlagte reisen din',
           'About your planed trip',
           'Om den planlagde reisa di',
         ),
