8b2f1a3c39162030d44b768464505745ec59b725
Jonas Brunvoll Larsson <jbl@variant.no>
2024-11-21 14:59:20 +0100

    remove group travel endpoint.

--- src/page-modules/contact/server/contact/index.ts
+++ src/page-modules/contact/server/contact/index.ts
@@ -5,7 +5,6 @@ export type ContactApi = {
   submitTicketControlForm(formData: any): Promise<ContactApiReturnType>;
   submitTravelGuaranteeForm(formData: any): Promise<ContactApiReturnType>;
   submitMeansOfTransportForm(formData: any): Promise<ContactApiReturnType>;
-  submitGroupTravelForm(formData: any): Promise<ContactApiReturnType>;
   submitTicketingForm(formData: any): Promise<ContactApiReturnType>;
 };
 
@@ -33,15 +32,6 @@ export function createContactApi(
 
     async submitMeansOfTransportForm(formData) {
       const response = await request('/means-of-transport', {
-        method: 'POST',
-        body: formData,
-      });
-      const data: ContactApiReturnType = await response.json();
-      return data;
-    },
-
-    async submitGroupTravelForm(formData) {
-      const response = await request('/group-travel', {
         method: 'POST',
         body: formData,
       });
