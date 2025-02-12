eec19df6a491fcaf0cfa2626bc2c35cd02c30f55
Jonas Brunvoll Larsson <59939294+jonasbrunvoll@users.noreply.github.com>
2025-02-12 10:16:51 +0100

    fix: update state machine of the ticket control page in the contact form  (#490)
    * Update ticket control state machine.
    
    * Remove console.log

--- src/page-modules/contact/ticket-control/events.ts
+++ src/page-modules/contact/ticket-control/events.ts
@@ -1,23 +1,31 @@
 import { commonEvents } from '../commoneEvents';
+import { FormType } from './ticket-control-form-machine';
 
-const ticketControlSpecificFormEvents = {} as {
-  type: 'ON_INPUT_CHANGE';
-  inputName:
-    | 'formType'
-    | 'feeNumber'
-    | 'invoiceNumber'
-    | 'appPhoneNumber'
-    | 'customerNumber'
-    | 'travelCardNumber'
-    | 'isAppTicketStorageMode'
-    | 'agreesFirstAgreement'
-    | 'agreesSecondAgreement'
-    | 'hasInternationalBankAccount'
-    | 'dateOfTicketControl'
-    | 'timeOfTicketControl';
-  value: string | boolean;
-};
-
+const ticketControlSpecificFormEvents = {} as
+  | {
+      type: 'ON_INPUT_CHANGE';
+      inputName:
+        | 'formType'
+        | 'feeNumber'
+        | 'invoiceNumber'
+        | 'appPhoneNumber'
+        | 'customerNumber'
+        | 'travelCardNumber'
+        | 'isAppTicketStorageMode'
+        | 'agreesFirstAgreement'
+        | 'agreesSecondAgreement'
+        | 'hasInternationalBankAccount'
+        | 'dateOfTicketControl'
+        | 'timeOfTicketControl';
+      value: string | boolean;
+    }
+  | {
+      type: 'SELECT_FORM_TYPE';
+      formType: FormType;
+    }
+  | {
+      type: 'SUBMIT';
+    };
 export const ticketControlFormEvents = {} as
   | typeof ticketControlSpecificFormEvents
   | typeof commonEvents;
