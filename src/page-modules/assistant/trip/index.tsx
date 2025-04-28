da1a203b1eb55b0500e4fb8253dec72cda5cdf39
Jorge Osorio <43450882+jorelosorio@users.noreply.github.com>
2025-04-28 14:52:42 +0200

    fix: Colors and buttons fixes (#507)
    * Update labled-input.module.css
    
    * Travel options filter fixes
    
    * Update situation.module.css
    
    * Update filter.module.css
    
    * Update message-box.module.css
    
    * chore: Revert color changes
    
    * revert
    
    * Update planner-web.css
    
    * Update planner-web.css
    
    * chore: New widget version

--- src/page-modules/assistant/trip/index.tsx
+++ src/page-modules/assistant/trip/index.tsx
@@ -149,6 +149,8 @@ export default function Trip({ tripQuery, fallback }: TripProps) {
         <Button
           className={style.fetchButton}
           onClick={() => loadMore()}
+          mode="secondary"
+          radiusSize="circular"
           title={t(PageText.Assistant.trip.fetchMore)}
           state={isLoadingMore ? 'loading' : undefined}
           testID="loadMoreButton"
