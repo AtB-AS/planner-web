5936d426a0e5800a74f9c882601868cd1fe19d1e
Jonas Brunvoll Larsson <59939294+jonasbrunvoll@users.noreply.github.com>
2025-05-07 10:57:01 +0200

    feat: extend textarea component in contact from  (#546)
    * Move FileInput and description of textarea to inside the textarea component
    
    * To make the display of the component in the forms more visually pleasing

--- src/page-modules/contact/ticketing/forms/app/appAccountForm.tsx
+++ src/page-modules/contact/ticketing/forms/app/appAccountForm.tsx
@@ -1,8 +1,7 @@
 import { PageText, useTranslation } from '@atb/translations';
 import { ticketingFormEvents } from '../../events';
 import { TicketingContextType } from '../../ticketingStateMachine';
-import { Typo } from '@atb/components/typography';
-import { Fieldset, Input, Textarea, FileInput } from '../../../components';
+import { Fieldset, Input, Textarea } from '../../../components';
 
 type AppAccountFormProps = {
   state: { context: TicketingContextType };
@@ -15,11 +14,9 @@ export const AppAccountForm = ({ state, send }: AppAccountFormProps) => {
   return (
     <>
       <Fieldset title={t(PageText.Contact.input.question.title)}>
-        <Typo.p textType="body__primary">
-          {t(PageText.Contact.input.question.info)}
-        </Typo.p>
         <Textarea
           id="question"
+          description={t(PageText.Contact.input.question.info)}
           value={state.context.question || ''}
           onChange={(e) =>
             send({
@@ -33,16 +30,16 @@ export const AppAccountForm = ({ state, send }: AppAccountFormProps) => {
               ? t(state.context.errorMessages['question']?.[0])
               : undefined
           }
-        />
-        <FileInput
-          name="attachments"
-          label={t(PageText.Contact.input.question.attachment)}
-          onChange={(files) => {
-            send({
-              type: 'ON_INPUT_CHANGE',
-              inputName: 'attachments',
-              value: files,
-            });
+          fileInputProps={{
+            name: 'attachments',
+            label: t(PageText.Contact.input.question.attachment),
+            onChange: (files) => {
+              send({
+                type: 'ON_INPUT_CHANGE',
+                inputName: 'attachments',
+                value: files,
+              });
+            },
           }}
         />
       </Fieldset>
