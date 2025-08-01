b6726771baa36cf921b9eb5cfa2221a8b0bfb6ac
Håkon Strandlie <hakon.strandlie@atb.no>
2025-08-01 15:20:48 +0200

    fix: Use cluster internal URL to BFF (#604)
    * fix: Use cluster internal URL to BFF
    
    * test: Fixed test

---
+++ src/utils/env.ts
@@ -0,0 +1,54 @@
+import 'dotenv/config';
+
+/**
+ * The base URL for the API, used in development mode. Use `getServiceUrl` to
+ * connect directly to service in production.
+ */
+const API_BASE_URL: string | undefined = process.env.API_BASE_URL;
+
+export function getEnv() {
+  return process.env['NODE_ENV'] === 'production' ? 'prod' : 'dev';
+}
+
+/**
+ * Return a URL to the given service. `serviceKey` should be uppercased.
+ */
+export const getServiceUrl = (
+  prefix: string,
+  serviceKey: string,
+  required: boolean = false,
+): string => {
+  if (getEnv() === 'dev') return API_BASE_URL ?? '';
+
+  const host = getServiceHost(serviceKey);
+  const port = getServicePort(serviceKey);
+
+  if (host === '' || port === '') {
+    if (required) {
+      console.error(
+        `failed to read environment variables for required service: ${serviceKey}`,
+      );
+      process.exit(-1);
+    } else {
+      return '';
+    }
+  }
+
+  return `${prefix}${host}:${port}`;
+};
+
+const getServiceHost = (serviceKey: string): string => {
+  return (
+    process.env[`${serviceKey}_SERVICE_SERVICE_HOST`] ||
+    process.env[`${serviceKey}_SERVICE_HOST`] ||
+    ''
+  );
+};
+
+const getServicePort = (serviceKey: string): string => {
+  return (
+    process.env[`${serviceKey}_SERVICE_SERVICE_PORT`] ||
+    process.env[`${serviceKey}_SERVICE_PORT`] ||
+    ''
+  );
+};
