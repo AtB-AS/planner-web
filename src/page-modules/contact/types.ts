cb69cbe4140cbdc6b41d32bfbbe541ed0d687826
Jonas Brunvoll Larsson <jbl@variant.no>
2025-08-04 14:29:48 +0200

    Update TicketType to use TicketTypeId enum.

--- src/page-modules/contact/types.ts
+++ src/page-modules/contact/types.ts
@@ -2,7 +2,18 @@ import { TranslatedString } from '@atb/translations';
 
 export type TransportModeType = 'bus' | 'expressboat' | 'ferry';
 export type ReasonForTransportFailure = { id: string; name: TranslatedString };
-export type TicketType = { id: string; name: TranslatedString };
+export enum TicketTypeId {
+  SingleTicket = 'singleTicket',
+  PeriodTicket = 'periodTicket',
+  FramYoung = 'framYoung',
+  FramStudent = 'framStudent',
+  FramAdult = 'framAdult',
+  FramSenior = 'framSenior',
+}
+export type TicketType = {
+  id: TicketTypeId;
+  name: TranslatedString;
+};
 export type PurchasePlatformType =
   | 'framApp'
   | 'enturApp'
