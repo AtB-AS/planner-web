05ef1f3e2bc1fab94770b770247afbed29127ace
Mikael Brevik <mikaelbre@gmail.com>
2025-08-14 13:10:58 +0200

    feat: adds personalia as input for app refund (#620)

--- src/page-modules/contact/refund/forms/refund-ticket/appTicketRefund.tsx
+++ src/page-modules/contact/refund/forms/refund-ticket/appTicketRefund.tsx
@@ -122,4 +122,142 @@ export const AppTicketRefund = ({ state, send }: AppTicketRefundProps) => {
   );
 };
 
-export default AppTicketRefund;
+type AboutYouSectionProps = Pick<AppTicketRefundProps, 'state' | 'send'>;
+
+const AboutYouSection = ({ state, send }: AboutYouSectionProps) => {
+  const { t } = useTranslation();
+  return (
+    <Fieldset title={t(PageText.Contact.aboutYouInfo.title)}>
+      <Input
+        id="firstName"
+        label={t(PageText.Contact.input.firstName.label)}
+        type="text"
+        autoComplete="given-name additional-name"
+        name="firstName"
+        value={state.context.firstName || ''}
+        isRequired
+        errorMessage={state.context?.errorMessages['firstName']?.[0]}
+        onChange={(e) =>
+          send({
+            type: 'ON_INPUT_CHANGE',
+            inputName: 'firstName',
+            value: e.target.value,
+          })
+        }
+      />
+
+      <Input
+        id="lastName"
+        label={t(PageText.Contact.input.lastName.label)}
+        type="text"
+        autoComplete="family-name"
+        name="lastName"
+        value={state.context.lastName || ''}
+        isRequired
+        errorMessage={state.context?.errorMessages['lastName']?.[0]}
+        onChange={(e) =>
+          send({
+            type: 'ON_INPUT_CHANGE',
+            inputName: 'lastName',
+            value: e.target.value,
+          })
+        }
+      />
+      <Input
+        id="address"
+        label={t(PageText.Contact.input.address.label)}
+        type="text"
+        autoComplete="street-address"
+        name="address"
+        value={state.context.address || ''}
+        isRequired
+        errorMessage={state.context?.errorMessages['address']?.[0]}
+        onChange={(e) =>
+          send({
+            type: 'ON_INPUT_CHANGE',
+            inputName: 'address',
+            value: e.target.value,
+          })
+        }
+      />
+      <Input
+        id="postalCode"
+        label={t(PageText.Contact.input.postalCode.label)}
+        type="text"
+        autoComplete="postal-code"
+        name="postalCode"
+        value={state.context.postalCode || ''}
+        isRequired
+        errorMessage={state.context?.errorMessages['postalCode']?.[0]}
+        onChange={(e) =>
+          send({
+            type: 'ON_INPUT_CHANGE',
+            inputName: 'postalCode',
+            value: e.target.value,
+          })
+        }
+      />
+      <Input
+        id="city"
+        label={t(PageText.Contact.input.city.label)}
+        type="text"
+        name="city"
+        value={state.context.city || ''}
+        isRequired
+        errorMessage={state.context?.errorMessages['city']?.[0]}
+        onChange={(e) =>
+          send({
+            type: 'ON_INPUT_CHANGE',
+            inputName: 'city',
+            value: e.target.value,
+          })
+        }
+      />
+      <Input
+        id="email"
+        label={t(PageText.Contact.input.email.label)}
+        type="email"
+        autoComplete="email"
+        name="email"
+        value={state.context.email || ''}
+        isRequired
+        errorMessage={state.context?.errorMessages['email']?.[0]}
+        onChange={(e) =>
+          send({
+            type: 'ON_INPUT_CHANGE',
+            inputName: 'email',
+            value: e.target.value,
+          })
+        }
+      />
+      <Input
+        id="phoneNumber"
+        label={t(PageText.Contact.input.phoneNumber.label)}
+        type="tel"
+        name="phoneNumber"
+        value={state.context.phoneNumber || ''}
+        isRequired
+        errorMessage={state.context?.errorMessages['phoneNumber']?.[0]}
+        onChange={(e) =>
+          send({
+            type: 'ON_INPUT_CHANGE',
+            inputName: 'phoneNumber',
+            value: e.target.value,
+          })
+        }
+      />
+    </Fieldset>
+  );
+};
+
+export default function AppTicketRefundContent({
+  state,
+  send,
+}: AppTicketRefundProps) {
+  return (
+    <>
+      <AppTicketRefund state={state} send={send} />
+      <AboutYouSection state={state} send={send} />
+    </>
+  );
+}
