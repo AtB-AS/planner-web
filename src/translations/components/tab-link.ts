193c7c53b152a6f6400a54bbfa037b3cf1d9ea4d
Christian Brevik <christian.brevik@gmail.com>
2025-09-10 14:19:52 +0200

    fix: Update to See departures

--- src/translations/components/tab-link.ts
+++ src/translations/components/tab-link.ts
@@ -2,5 +2,5 @@ import { translation as _ } from '@atb/translations/commons';
 
 export const TabLink = {
   assistant: _('Finn reise', 'Find journey', 'Finn reise'),
-  departures: _('Se avganger', 'Departures', 'Sjå avganger'),
+  departures: _('Se avganger', 'See departures', 'Sjå avganger'),
 };
