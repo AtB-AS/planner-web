066c732554711b6b1c9db38f4cbaf781195eb160
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2026-02-19 12:40:39 +0100

    refactor(contact): remove contact page-module UI and state machines

--- src/page-modules/contact/index.ts
+++
@@ -1,8 +0,0 @@
-export { type Line } from './server/journey-planner/validators';
-export { shouldShowContactPage } from './utils';
-export { ContactPageLayout, type ContactPageLayoutProps } from './layouts';
-export { default as RefundContent } from './refund';
-export { default as MeansOfTransportContent } from './means-of-transport';
-export { default as TicketControlPageContent } from './ticket-control';
-export { default as TicketingContent } from './ticketing';
-export { default as JourneyInfoContent } from './journey-info';
