fc1f93fc140ffe1282c1fa5ed5da9e600392d269
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2026-02-19 12:40:39 +0100

    refactor(contact): remove contact page-module UI and state machines

--- src/page-modules/contact/means-of-transport/forms/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as DelayForm } from './delayForm';
-export { default as DriverForm } from './driverForm';
-export { default as InjuryForm } from './injuryForm';
-export { default as ServiceOfferingForm } from './serviceOfferingForm';
-export { default as StopForm } from './stopForm';
-export { default as TransportationForm } from './transportationForm';
