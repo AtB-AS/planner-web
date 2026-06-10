e5eb634c83ddf1749cee922c50f0734b9233c6e5
Håkon Strandlie <hakon.strandlie@atb.no>
2026-06-10 14:12:05 +0200

    refactor: extract dynamic map import to remove circular dependency

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,15 +1,9 @@
-import dynamic from 'next/dynamic';
-import MapLoading from './map-loading';
-
 export { MapWithHeader } from './map-with-header';
 export { MapHeader } from './map-header';
 
 export * from './utils';
 export * from './types';
 
-export const Map = dynamic(() => import('./map'), {
-  ssr: false,
-  loading: () => <MapLoading />,
-});
+export { Map } from './dynamic-map';
 
 export { getStaticMapUrl } from './static-map';
