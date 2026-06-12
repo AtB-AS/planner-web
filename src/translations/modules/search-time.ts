46c2257f78fb3a1c1f4ec79c46d404bdffa891aa
Johannes Røsvik <j.rosvik@gmail.com>
2026-06-12 12:40:27 +0200

    feat: update SearchTime selection text

--- src/translations/modules/search-time.ts
+++ src/translations/modules/search-time.ts
@@ -1,7 +1,7 @@
 import { translation as _ } from '@atb/translations/commons';
 
 export const SearchTime = {
-  now: _('Dra nå', 'Leave now', 'Dra no'),
+  now: _('Nå', 'Now', 'No'),
   departure: _('Avreise', 'Leave at', 'Avreise'),
   arrival: _('Ankomst', 'Arrive by', 'Framkomst'),
   date: _('Dato', 'Date', 'Dato'),
