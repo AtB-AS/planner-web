7d4ab5c9524d3527fd588c92d4f61f7fad90b6d8
Mathias Oterhals Myklebust <mom@variant.no>
2026-09-01 16:12:52 +0200

    refactor: remove getContactPageTitle

--- src/server/contact/utils.ts
+++ src/server/contact/utils.ts
@@ -1,12 +1,2 @@
-import { PageText, TranslatedString } from '@atb/translations';
-
-export const getContactPageTitle = (
-  subtitle?: TranslatedString,
-): TranslatedString[] => {
-  return subtitle
-    ? [subtitle, PageText.Contact.pageTitle]
-    : [PageText.Contact.pageTitle];
-};
-
 export const shouldShowContactPage = (): boolean =>
   !!process.env.NEXT_PUBLIC_CONTACT_API_URL;
