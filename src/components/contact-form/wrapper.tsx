1e9d46dd02865d00eec069f9ed8bb7165dfe96db
Mathias Oterhals Myklebust <mom@variant.no>
2026-09-01 16:08:38 +0200

    refactor: toContactFormLanguage

--- src/components/contact-form/wrapper.tsx
+++ src/components/contact-form/wrapper.tsx
@@ -3,24 +3,32 @@ import {
   Language as ContactFormLanguage,
 } from '@mrfylke/contact-form';
 import { contactFormConfig } from '@atb/config/contact-form-config';
-import { useLanguageSettings } from '@atb/translations';
+import { Language, useLanguageSettings } from '@atb/translations';
 import { useDarkMode } from '@atb/modules/theme';
+import { PropsWithChildren } from 'react';
 
-type ContactFormWrapperProps = {
-  children: React.ReactNode;
-};
-
-export function ContactFormWrapper({ children }: ContactFormWrapperProps) {
+export function ContactFormWrapper({ children }: PropsWithChildren) {
   const { language } = useLanguageSettings();
   const [isDarkMode] = useDarkMode();
 
   return (
     <ContactFormRoot
       config={contactFormConfig}
-      language={language as ContactFormLanguage}
+      language={toContactFormLanguage(language)}
       theme={isDarkMode ? 'dark' : 'light'}
     >
       {children}
     </ContactFormRoot>
   );
+}
+
+function toContactFormLanguage(language: Language): ContactFormLanguage {
+  switch (language) {
+    case Language.English:
+      return ContactFormLanguage.English;
+    case Language.Norwegian:
+      return ContactFormLanguage.Norwegian;
+    case Language.Nynorsk:
+      return ContactFormLanguage.Nynorsk;
+  }
 }
