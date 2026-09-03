be338b4341de33de35f50a6d089d213b98e81697
Gøran Dalum <goran.dalum@gmail.com>
2026-09-03 07:28:32 +0200

    prettier

--- src/pages/dev/trip-pattern.tsx
+++ src/pages/dev/trip-pattern.tsx
@@ -444,9 +444,7 @@ const DevTripPatternPage: NextPage<DevTripPatternPageProps> = (props) => {
       <footer className={style.buildInfo}>
         <span>org: {process.env.NEXT_PUBLIC_PLANNER_ORG_ID}</span>
         <span>env: {process.env.NEXT_PUBLIC_ENVIRONMENT || '-'}</span>
-        <span>
-          firebase: {process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}
-        </span>
+        <span>firebase: {process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}</span>
         <span>build: {process.env.NEXT_PUBLIC_BUILD_ID || '-'}</span>
       </footer>
     </DefaultLayout>
